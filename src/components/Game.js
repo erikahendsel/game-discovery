7f34c7e1297e7b1212014640049c30f1b09f5bfc
Your Name <erikahendsel@hotmail.com>
2020-12-21 20:50:09 +0100

    heart icon added for each card.-005

--- src/components/Game.js
+++ src/components/Game.js
@@ -6,6 +6,9 @@ import { useDispatch } from "react-redux";
 import { loadDetail } from "../actions/detailAction";
 import { Link } from "react-router-dom";
 import { smallImage } from "../util";
+//Icon
+/* Helpful article about importing svg: https://dev.to/abachi/how-to-change-svg-s-color-in-react-42g2 */
+import { ReactComponent as HeartIcon } from "../img/heart-icon.svg";
 
 const Game = ({ name, released, image, id }) => {
   //Load Detail Handler
@@ -23,11 +26,16 @@ const Game = ({ name, released, image, id }) => {
         </div>
         <img src={smallImage(image, 640)} alt={name} />
       </Link>
+      <FavoriteStyled>
+        {/* Helpful article about importing svg: https://dev.to/abachi/how-to-change-svg-s-color-in-react-42g2 */}
+        <HeartIcon />
+      </FavoriteStyled>
     </GameStyled>
   );
 };
 
 const GameStyled = styled.div`
+  position: relative;
   min-height: 20vh;
   background: #4a4d54;
   text-align: center;
@@ -47,4 +55,20 @@ const GameStyled = styled.div`
   }
 `;
 
+const FavoriteStyled = styled.div`
+  position: absolute;
+  z-index: 3;
+  bottom: 0;
+  right: 0;
+  background: rgba(47, 49, 54, 0.7);
+  padding: 0.3em 0.9em;
+  border: 1px solid rgba(255, 255, 255, 0.2);
+  transition: ease all 0.2s;
+  &:hover {
+    background: rgba(47, 49, 54, 1);
+    .heart-icon {
+      fill: red;
+    }
+  }
+`;
 export default Game;
